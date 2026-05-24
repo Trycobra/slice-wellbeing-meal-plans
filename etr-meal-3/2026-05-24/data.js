b8b60646b57e8c068b56b57da136ef290a022112
Trycobra <103174842+Trycobra@users.noreply.github.com>
2026-05-24 23:22:55 +0700

    Add original ETR data

---
+++ etr-meal-3/2026-05-24/data.js
@@ -0,0 +1,44 @@
+var DATA = {
+  Monday: {
+    breakfast: { P:44,   F:6,    C:43,   Cal:410, name:'Jamaican Jerk Chicken Breast (Harissa)' },
+    lunch:     { P:30,   F:9,    C:46,   Cal:385, name:'BBQ Rub NZ Tenderloin (Peri Peri Dressing)' },
+    dinner:    { P:26,   F:11,   C:40,   Cal:363, name:'NZ Ground Beef Kra Pao (Fragrant Thai Dressing)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  },
+  Tuesday: {
+    breakfast: { P:28,   F:2.6,  C:39,   Cal:285, name:'Tikka Masala Chicken Breast (Chimichurri)' },
+    lunch:     { P:32,   F:10,   C:42,   Cal:386, name:'Masala Beef Tenderloin (Soy-Ginger)' },
+    dinner:    { P:32,   F:3,    C:59,   Cal:410, name:'Mahi Mahi with Pasta and Carrots (Apple Cider Vinegar)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  },
+  Wednesday: {
+    breakfast: { P:31,   F:8,    C:36,   Cal:365, name:'Minced Chicken Kra Pao (Apple Cider Vinegar)' },
+    lunch:     { P:27,   F:8,    C:41,   Cal:360, name:'Panang Beef NZ Tenderloin (Peri Peri Dressing)' },
+    dinner:    { P:33,   F:7,    C:31,   Cal:325, name:'Togarashi Seabass (Fragrant Thai Dressing)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  },
+  Thursday: {
+    breakfast: { P:42,   F:6,    C:51,   Cal:430, name:'Moroccan Chicken Breast (Sesame Turmeric Dressing)' },
+    lunch:     { P:32,   F:3,    C:59,   Cal:410, name:'Mahi Mahi with Pasta and Carrots (Chimichurri)' },
+    dinner:    { P:31,   F:8,    C:36,   Cal:365, name:'Minced Chicken Kra Pao (Fragrant Thai Dressing)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  },
+  Friday: {
+    breakfast: { P:26,   F:11,   C:40,   Cal:363, name:'NZ Ground Beef Kra Pao (Peri Peri Dressing)' },
+    lunch:     { P:28,   F:2.6,  C:39,   Cal:285, name:'Tikka Masala Chicken Breast (Harissa)' },
+    dinner:    { P:36,   F:3,    C:39,   Cal:330, name:'Togarashi Tuna with Soba Noodles & Zucchini (Soba Dressing)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  },
+  Saturday: {
+    breakfast: { P:42,   F:6,    C:51,   Cal:430, name:'Moroccan Chicken Breast (Harissa)' },
+    lunch:     { P:38,   F:4,    C:38,   Cal:340, name:'Tuna with Brown Rice & Asparagus (Sesame Turmeric Dressing)' },
+    dinner:    { P:44,   F:6,    C:43,   Cal:410, name:'Jamaican Jerk Chicken Breast (Peri Peri Dressing)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  },
+  Sunday: {
+    breakfast: { P:33,   F:7,    C:31,   Cal:325, name:'Togarashi Seabass (Apple Cider Vinegar)' },
+    lunch:     { P:29,   F:16,   C:34,   Cal:400, name:'Cajun Salmon with Cauli-Quinoa & Carrot (Soy-Ginger)' },
+    dinner:    { P:31,   F:16,   C:46,   Cal:465, name:'Teriyaki Salmon with Rice Berry & Broccoli (Teriyaki Sauce)' },
+    snack:     { P:0,    F:0,    C:0,    Cal:0,   name:'' }
+  }
+};
