fe3a3a852a7e6b6be469639e58b49b833656effd
Trycobra <103174842+Trycobra@users.noreply.github.com>
2026-05-24 23:43:25 +0700

    Add original dinner is served western

---
+++ dinner-is-served-western/2026/05/24/data.js
@@ -0,0 +1,155 @@
+<tbody>
+  <!-- ===== MEAL 1 (Dinner) ===== -->
+        <tr id="tr-dinner-name">
+          <td rowspan="3" class="section-title">Meal 1</td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Monday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Jerk-Salmon-Coconut-Rice-Pineapple-Salsa.jpg" alt="">Jerk Salmon with Coconut Rice &amp; Pineapple Salsa
+          </td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Tuesday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/PeriPeri-Chicken-Sweet-Potato-Roasted-Corn-Salsa.jpg" alt="">Peri Peri Chicken with Sweet Potato &amp; Roasted Corn Salsa
+          </td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Wednesday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Beef-Shepherd-Pie-Cauliflower-Mash.jpg" alt="">Beef Shepherd's Pie (Cauliflower Mash)
+          </td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Thursday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Chicken-Tikka-Masala.jpg" alt="">Chicken Tikka Masala with Brown &amp; White Rice (60/40) 100g
+          </td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Friday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Chicken-Tacos.jpg" alt="">Mexican Chicken Taco
+          </td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Saturday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Classic-Lasagna.jpg" alt="">Classic Lasagna
+          </td>
+          <td class="meal-cell noborder-bottom mealtxt" data-day="Sunday" data-mealtype="dinner">
+            <img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Beef-Broccoli.jpg" alt="">Beef and Broccoli Stir-fry with Brown &amp; White Rice (60/40) 100g
+          </td>
+        </tr>
+        <tr id="tr-dinner-macro">
+          <td class="macrotxt" data-day="Monday">P 54g / F 41g / C 51g / Fiber 4g / Cal 746</td>
+          <td class="macrotxt" data-day="Tuesday">P 41g / F 42g / C 40g / Fiber 5g / Cal 620</td>
+          <td class="macrotxt" data-day="Wednesday">P 37g / F 33g / C 7g / Fiber 5g / Cal 500</td>
+          <td class="macrotxt" data-day="Thursday">P 30g / F 23.9g / C 46g / Fiber 4.5g / Cal 522</td>
+          <td class="macrotxt" data-day="Friday">P 60g / F 30g / C 13g / Fiber 9g / Cal 562</td>
+          <td class="macrotxt" data-day="Saturday">P 38g / F 33g / C 7g / Fiber 5g / Cal 500</td>
+          <td class="macrotxt" data-day="Sunday">P 40.5g / F 31.1g / C 40.1g / Fiber 12.3g / Cal 622</td>
+        </tr>
+        <tr id="tr-dinner-cb">
+          <td class="checkbox-cell chkbx" data-day="Monday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Monday" data-mealtype="dinner" value="Monday|Meal 1|Jerk Salmon with Coconut Rice & Pineapple Salsa"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Monday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+          <td class="checkbox-cell chkbx" data-day="Tuesday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Tuesday" data-mealtype="dinner" value="Tuesday|Meal 1|Peri Peri Chicken with Sweet Potato & Roasted Corn Salsa"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Tuesday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+          <td class="checkbox-cell chkbx" data-day="Wednesday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Wednesday" data-mealtype="dinner" value="Wednesday|Meal 1|Beef Shepherd's Pie (Cauliflower Mash)"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Wednesday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+          <td class="checkbox-cell chkbx" data-day="Thursday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Thursday" data-mealtype="dinner" value="Thursday|Meal 1|Chicken Tikka Masala with Brown & White Rice (60/40) 100g"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Thursday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+          <td class="checkbox-cell chkbx" data-day="Friday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Friday" data-mealtype="dinner" value="Friday|Meal 1|Mexican Chicken Taco"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Friday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+          <td class="checkbox-cell chkbx" data-day="Saturday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Saturday" data-mealtype="dinner" value="Saturday|Meal 1|Classic Lasagna"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Saturday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+          <td class="checkbox-cell chkbx" data-day="Sunday">
+            <label class="checkbox-wrap"><input type="checkbox" class="meal-checkbox" data-day="Sunday" data-mealtype="dinner" value="Sunday|Meal 1|Beef and Broccoli Stir-fry with Brown & White Rice (60/40) 100g"> <span>Select</span></label>
+            <div class="qty-wrap" data-day="Sunday" data-mealtype="dinner">
+              <button type="button" class="qty-btn qty-minus" disabled>−</button>
+              <input type="text" inputmode="numeric" pattern="[0-9]*" class="qty-input" value="1" disabled>
+              <button type="button" class="qty-btn qty-plus" disabled>+</button>
+            </div>
+          </td>
+        </tr>
+
+        <!-- ===== DESSERT (always bundled) ===== -->
+        <tr id="tr-dessert-name">
+          <td rowspan="2" class="section-title">Dessert</td>
+          <td class="noborder-bottom mealtxt" data-day="Monday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Coco-Mango-Chia-VGTRN.jpg" alt="">Coconut &amp; Mango Chia Pudding</td>
+          <td class="noborder-bottom mealtxt" data-day="Tuesday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/preset-default.jpg" alt="">Peanut Butter Berry Overnight Oats</td>
+          <td class="noborder-bottom mealtxt" data-day="Wednesday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Matcha-Granola-Chia-Pudding-VGTRN.jpg" alt="">Matcha Granola Chia Pudding</td>
+          <td class="noborder-bottom mealtxt" data-day="Thursday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/preset-default.jpg" alt="">Blueberry Cashew Cheesecake</td>
+          <td class="noborder-bottom mealtxt" data-day="Friday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Peanut-Butter-Cookies.jpg" alt="">Peanut Butter Cookie</td>
+          <td class="noborder-bottom mealtxt" data-day="Saturday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/preset-default.jpg" alt="">Chocolate Banana Chia Pudding</td>
+          <td class="noborder-bottom mealtxt" data-day="Sunday"><img class="mealimg" src="https://staging.slicewellbeing.com/wp-content/uploads/2026/04/Blueberry-Fig-Yoghurt-Cup.jpg" alt="">Blueberry Fig Yoghurt Cup</td>
+        </tr>
+        <tr id="tr-dessert-macro">
+          <td class="macrotxt" data-day="Monday">P 18g / F 14g / C 28g / Fiber 2g / Cal 310</td>
+          <td class="macrotxt" data-day="Tuesday">P 3.8g / F 4.5g / C 18g / Fiber 4g / Cal 120</td>
+          <td class="macrotxt" data-day="Wednesday">P 6g / F 12g / C 26g / Fiber 7g / Cal 230</td>
+          <td class="macrotxt" data-day="Thursday">P 7g / F 42g / C 12g / Fiber 4g / Cal 450</td>
+          <td class="macrotxt" data-day="Friday">P 3g / F 8g / C 10g / Fiber 2g / Cal 120</td>
+          <td class="macrotxt" data-day="Saturday">P 3g / F 13g / C 22g / Fiber 6g / Cal 210</td>
+          <td class="macrotxt" data-day="Sunday">P 11.3g / F 13.6g / C 47.4g / Fiber 8.4g / Cal 393</td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
+</div>
+
+  </tbody>
+
+<script>
+  var DATA = {
+  Monday: {
+    dinner:  { P:54,   F:41,   C:51,   Cal:746, name:'Jerk Salmon with Coconut Rice & Pineapple Salsa' },
+    dessert: { P:18,   F:14,   C:28,   Cal:310, name:'Coconut & Mango Chia Pudding' }
+  },
+  Tuesday: {
+    dinner:  { P:41,   F:42,   C:40,   Cal:620, name:'Peri Peri Chicken with Sweet Potato & Roasted Corn Salsa' },
+    dessert: { P:3.8,  F:4.5,  C:18,   Cal:120, name:'Peanut Butter Berry Overnight Oats' }
+  },
+  Wednesday: {
+    dinner:  { P:37,   F:33,   C:7,    Cal:500, name:"Beef Shepherd's Pie (Cauliflower Mash)" },
+    dessert: { P:6,    F:12,   C:26,   Cal:230, name:'Matcha Granola Chia Pudding' }
+  },
+  Thursday: {
+    dinner:  { P:30,   F:23.9, C:46,   Cal:522, name:'Chicken Tikka Masala with Brown & White Rice (60/40) 100g' },
+    dessert: { P:7,    F:42,   C:12,   Cal:450, name:'Blueberry Cashew Cheesecake' }
+  },
+  Friday: {
+    dinner:  { P:60,   F:30,   C:13,   Cal:562, name:'Mexican Chicken Taco' },
+    dessert: { P:3,    F:8,    C:10,   Cal:120, name:'Peanut Butter Cookie' }
+  },
+  Saturday: {
+    dinner:  { P:38,   F:33,   C:7,    Cal:500, name:'Classic Lasagna' },
+    dessert: { P:3,    F:13,   C:22,   Cal:210, name:'Chocolate Banana Chia Pudding' }
+  },
+  Sunday: {
+    dinner:  { P:40.5, F:31.1, C:40.1, Cal:622, name:'Beef and Broccoli Stir-fry with Brown & White Rice (60/40) 100g' },
+    dessert: { P:11.3, F:13.6, C:47.4, Cal:393, name:'Blueberry Fig Yoghurt Cup' }
+  }
+};
+
+</script>
